6dc361382152cea4cad7b0267d22200120f15506
realhardworkingdeveloper <realhardworkingdeveloper@gmail.com>
2022-01-26 02:56:33 -0800

    rebuilt front-end and added breeding function

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Connection, PublicKey } from "@solana/web3.js";
 import { Program, Provider, web3 } from "@project-serum/anchor";
 import idl from "./idl.json";
 import { Button, Col, Container, Row } from "react-bootstrap";
+
+import nftLists from "./assets/nft-list.json";
+import NftListsModal from "./components/nft-lists-modal";
 
 import {
   PhantomWalletAdapter,
@@ -20,7 +23,10 @@ import {
 } from "@solana/wallet-adapter-react-ui";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
-const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
+const wallets = [
+  new PhantomWalletAdapter(),
+  // new SolflareWalletAdapter()
+];
 
 const { SystemProgram, Keypair } = web3;
 /* create an account  */
@@ -31,9 +37,16 @@ const opts = {
 const programID = new PublicKey(idl.metadata.address);
 
 const App = () => {
-  const [value, setValue] = useState(null);
-  const [firstImg, setFirstImg] = useState("");
-  const [secImg, setSecImg] = useState("");
+  const [breededAt, setBreededAt] = useState(null);
+  const [firstNft, setFirstNft] = useState("");
+  const [secNft, setSecNft] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [parent, setParent] = useState("");
+  const [filteredNftList, setFilteredNftList] = useState(nftLists);
+
+  useEffect(async () => {
+    await initialize();
+  }, []);
 
   const wallet = useWallet();
 
@@ -47,68 +60,128 @@ const App = () => {
     return provider;
   }
 
-  const handleImgUpload = async () => {
-    if (firstImg && secImg) {
+  async function initialize() {
+    const provider = await getProvider();
+    /* create the program interface combining the idl, program ID, and provider */
+    const program = new Program(idl, programID, provider);
+
+    try {
+      /* interact with the program via rpc */
+      const account = await program.account.baseAccount.fetch(
+        baseAccount.publicKey
+      );
+      console.log("account: ", account);
+      setBreededAt(account.breededAt);
+      setFirstNft(account.dataList.firtNft);
+      setSecNft(account.dataList.secNft);
+    } catch (err) {
+      console.log("Transaction error: ", err);
+    }
+  }
+
+  async function breeding() {
+    const nftWillBreed = [firstNft, secNft];
+    const provider = await getProvider();
+    /* create the program interface combining the idl, program ID, and provider */
+    const program = new Program(idl, programID, provider);
+    try {
+      /* interact with the program via rpc */
+      await program.rpc.breeding(nftWillBreed, {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount],
+      });
+
+      const account = await program.account.baseAccount.fetch(
+        baseAccount.publicKey
+      );
+      console.log("account: ", account);
+      setBreededAt(account.breededAt);
+      setFirstNft(account.dataList.firtNft);
+      setSecNft(account.dataList.secNft);
+    } catch (err) {
+      console.log("Transaction error: ", err);
+    }
+  }
+
+  const handleBreeding = async () => {
+    if (firstNft && secNft) {
+      await breeding();
     } else {
       alert("Select two images!");
     }
   };
 
-  async function onImgChange(e, setImg) {
-    const file = e.target.files[0];
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => {
-      setImg(reader.result);
-    };
-    reader.onerror = function (error) {
-      console.log("Error: ", error);
-    };
-  }
+  const selectNft = (parent) => {
+    setShowModal(true);
+    setParent(parent);
+  };
 
-  if (!wallet.connected) {
-    /* If the user's wallet is not connected, display connect wallet button. */
-    return (
+  const setParentNft = (index, id, nftLists) => {
+    console.log(index, id, nftLists);
+    const selectedNft = filteredNftList[index];
+    if (parent == "firstNft") setFirstNft(selectedNft.src);
+    else setSecNft(selectedNft.src);
+    // setFilteredNftList(nftLists.filter(x => x.id != id))
+    setShowModal(false);
+  };
+
+  return (
+    <div className="App">
       <div
         style={{
           display: "flex",
           justifyContent: "center",
-          marginTop: "100px",
+          marginTop: "30px",
         }}
       >
         <WalletMultiButton />
       </div>
-    );
-  } else {
-    return (
-      <div className="App">
-        <Container className="text-center">
-          <div className="mt-5 mb-2">
-            <h2>Please select images and click submit button.</h2>
-          </div>
-          <Row className="mt-5">
-            <Col md="6">
-              <div className="">
-                <img src={firstImg} />
-                <input type="file" onChange={(e) => onImgChange(e, setFirstImg)} />
-              </div>
-            </Col>
-            <Col md="6">
-              <div className="">
-                <img src={secImg} />
-                <input type="file" onChange={(e) => onImgChange(e, setSecImg)} />
-              </div>
-            </Col>
-          </Row>
-          <Row className="mt-5 justify-content-center">
-            <Col md="3">
-              <Button onClick={handleImgUpload}>Submit</Button>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+
+      <Container className="text-center">
+        <div className="mt-3 mb-2">
+          <h2>Please select NFTs and click submit button.</h2>
+        </div>
+        <Row className="mt-5">
+          <Col md="6">
+            <div className="">
+              <img
+                src={firstNft}
+                className="img-fluid img-thumbnail block-example border border-dark breeded-img"
+                onClick={() => selectNft("firstNft")}
+              />
+              <h3>Parent1</h3>
+            </div>
+          </Col>
+          <Col md="6">
+            <div className="">
+              <img
+                src={secNft}
+                className="img-fluid img-thumbnail block-example border border-dark breeded-img"
+                onClick={() => selectNft("secNft")}
+              />
+              <h3>Parent2</h3>
+            </div>
+          </Col>
+        </Row>
+        <Row className="mt-2 justify-content-center">
+          <Col md="3">
+            <Button onClick={handleBreeding}>Start Breeding</Button>
+          </Col>
+        </Row>
+      </Container>
+
+      <NftListsModal
+        nftLists={filteredNftList}
+        showModal={showModal}
+        setShowModal={setShowModal}
+        setParentNft={setParentNft}
+      />
+    </div>
+  );
 };
 
 /* wallet configuration as specified here: https://github.com/solana-labs/wallet-adapter#setup */
---
+++ app/src/components/nft-lists-modal.js
@@ -0,0 +1,46 @@
+import { Fragment, useState } from 'react';
+import { Button, Col, Modal, Row } from 'react-bootstrap';
+
+const NftListsModal = (props) => {
+    const { nftLists, setParentNft, showModal, setShowModal } = props;
+
+    return (
+        <Fragment>
+
+            <Modal
+                show={showModal}
+                onHide={() => setShowModal(false)}
+                dialogClassName="modal-90w"
+                aria-labelledby="example-custom-modal-styling-title"
+                size="lg"
+            // fullscreen={true}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="example-custom-modal-styling-title">
+                        Select NFT
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row>
+                        {
+                            nftLists.map((item, index) => {
+                                return (
+                                    <Col md="4" key={index}>
+                                        <img
+                                            src={item.src}
+                                            id={item.id}
+                                            className='img-fluid img-responsive cusor-pointer'
+                                            onClick={() => setParentNft(index, item.id, nftLists)}
+                                        />
+                                    </Col>
+                                )
+                            })
+                        }
+                    </Row>
+                </Modal.Body>
+            </Modal>
+        </Fragment>
+    )
+}
+
+export default NftListsModal;
